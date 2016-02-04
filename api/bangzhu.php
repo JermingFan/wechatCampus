47235b8582e003acb7d1ce9008ba79ba198fb8c6
JermingFan <jermingfan@foxmail.com>
2016-02-04 11:03:18 +0800

    update

--- api/bangzhu.php
+++ api/bangzhu.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 
-	$news = array();
-	$news[] = array('title' => "欢迎关注师大小伙伴^_^
+$news = array();
+$news[] = array('title' => "欢迎关注师大小伙伴^_^
 请按照各栏目提示进行使用", 'description' => '', 'picurl' =>'', 'url' =>'');
-	$news[] = array('title' => "【师大校园】--输入文字或对应数字\n
+$news[] = array('title' => "【师大校园】--输入文字或对应数字\n
     [1]师大要闻             [2]最新公告
     [3]考试成绩             [4]四六级
     [5]图书检索             [6]校历
     [7]校园地图             [#]取消绑定", 'description' => '', 'picurl' => '', 'url' => '');
-	$news[] = array('title' => "【师大生活】--输入对应文字或关键词\n
+$news[] = array('title' => "【师大生活】--输入对应文字或关键词\n
     ●快递                        ●二手
     ●天气                        ●失物
     ●百科                        ●翻译
     ●校花                        ●校草
     ●游戏                        ●网易头条", 'description' => '', 'picurl' => '', 'url' => '');
-	$news[] = array('title' => "【✐Tips】--输入[0]或[功能]返回主页
+$news[] = array('title' => "【✐Tips】--输入[0]或[功能]返回主页
 [注]:各手机消息排版可能不同,但不影响使用
 (WP系统微信版本较低，主页显示有所不同)", 'description' => '', 'picurl' => '', 'url' => '');
-	return $this->respNews($news);
+return $this->respNews($news);
 
 ?>
--- api/xiaohua.php
+++ api/xiaohua.php
@@ -4,90 +4,89 @@ header('Content-Type: text/html; charset=utf-8');
 
 $content = $this->message['content'];
 
-
 if(!strncasecmp($_SESSION['sel'], "校花", 6)&&!strncasecmp($content, "P", 1)){
-  $lines = $_SESSION['pages']*8+8;
-  $_SESSION['pages'] = $_SESSION['pages']+1;
-  $b = array();
-  $res = file_get_contents("http://www.facejoking.com/api/top/2016/0/1");
-  $xiaohua = json_decode($res,true);
-  for($id=$lines;$id<$lines+8;$id++){
-    $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
-    $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
-    $name = $xiaohua[data][$id][name];
-    $rank = $xiaohua[data][$id][rank];
-    if(!$name) break;
-    $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
-  }
-  if($xiaohua[data][$id][rank]) $b[]=array("title"=>'点击此处查看更多
+    $lines = $_SESSION['pages']*8+8;
+    $_SESSION['pages'] = $_SESSION['pages']+1;
+    $b = array();
+    $res = file_get_contents("http://www.facejoking.com/api/top/2016/0/1");
+    $xiaohua = json_decode($res,true);
+    for($id=$lines;$id<$lines+8;$id++){
+        $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
+        $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
+        $name = $xiaohua[data][$id][name];
+        $rank = $xiaohua[data][$id][rank];
+        if(!$name) break;
+        $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
+    }
+    if($xiaohua[data][$id][rank]) $b[]=array("title"=>'点击此处查看更多
 你还可以回复【校草】查看校草排名
 ---------------
 [输入0返回主页]',"url"=>'http://www.facejoking.com/top/2016/0/',"picurl"=>'',"description"=>'');
     else session_destroy();
-    return $this->respNews($b); 
-  }
+    return $this->respNews($b);
+}
 else if($content == '校花'){
-  $_SESSION['sel'] = '校花';
-  $_SESSION['pages'] = 0;
-  $b = array();
-  $res = file_get_contents("http://www.facejoking.com/api/top/2016/0/1");
-  $xiaohua = json_decode($res,true);
-  $b[]=array("title"=>'上师大校花TOP100：
+    $_SESSION['sel'] = '校花';
+    $_SESSION['pages'] = 0;
+    $b = array();
+    $res = file_get_contents("http://www.facejoking.com/api/top/2016/0/1");
+    $xiaohua = json_decode($res,true);
+    $b[]=array("title"=>'上师大校花TOP100：
   (数据实时同步facejoking)',"url"=>'',"picurl"=>'',"description"=>'');
-  for($id=0;$id<8;$id++){
-    $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
-    $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
-    $name = $xiaohua[data][$id][name];
-    $rank = $xiaohua[data][$id][rank];
-    $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
-  }
-  $b[]=array("title"=>'点击此处查看更多
+    for($id=0;$id<8;$id++){
+        $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
+        $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
+        $name = $xiaohua[data][$id][name];
+        $rank = $xiaohua[data][$id][rank];
+        $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
+    }
+    $b[]=array("title"=>'点击此处查看更多
 你还可以回复【校草】查看校草排名
 ---------------
 [输入0返回主页]',"url"=>'http://www.facejoking.com/top/2016/0/',"picurl"=>'',"description"=>'');
-    return $this->respNews($b); 
+    return $this->respNews($b);
 
 }
 if(!strncasecmp($_SESSION['sel'], "校草", 6)&&!strncasecmp($content, "P", 1)){
-  $lines = $_SESSION['pages']*8+8;
-  $_SESSION['pages'] = $_SESSION['pages']+1;
-  $b = array();
-  $res = file_get_contents("http://www.facejoking.com/api/top/2016/1/1");
-  $xiaohua = json_decode($res,true);
-  for($id=$lines;$id<$lines+8;$id++){
-    $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
-    $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
-    $name = $xiaohua[data][$id][name];
-    $rank = $xiaohua[data][$id][rank];
-    if(!$name) break;
-    $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
-  }
-  if($xiaohua[data][$id][rank]) $b[]=array("title"=>'点击此处查看更多
+    $lines = $_SESSION['pages']*8+8;
+    $_SESSION['pages'] = $_SESSION['pages']+1;
+    $b = array();
+    $res = file_get_contents("http://www.facejoking.com/api/top/2016/1/1");
+    $xiaohua = json_decode($res,true);
+    for($id=$lines;$id<$lines+8;$id++){
+        $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
+        $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
+        $name = $xiaohua[data][$id][name];
+        $rank = $xiaohua[data][$id][rank];
+        if(!$name) break;
+        $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
+    }
+    if($xiaohua[data][$id][rank]) $b[]=array("title"=>'点击此处查看更多
 你还可以回复【校花】查看校花排名
 ---------------
 [输入0返回主页]',"url"=>'http://www.facejoking.com/top/2016/1/',"picurl"=>'',"description"=>'');
-    return $this->respNews($b); 
-  }
+    return $this->respNews($b);
+}
 else if($content == '校草'){
-  $_SESSION['sel'] = '校草';
-  $_SESSION['pages'] = 0;
-  $b = array();
-  $res = file_get_contents("http://www.facejoking.com/api/top/2016/1/1");
-  $xiaohua = json_decode($res,true);
-  $b[]=array("title"=>'上师大校草TOP100：
+    $_SESSION['sel'] = '校草';
+    $_SESSION['pages'] = 0;
+    $b = array();
+    $res = file_get_contents("http://www.facejoking.com/api/top/2016/1/1");
+    $xiaohua = json_decode($res,true);
+    $b[]=array("title"=>'上师大校草TOP100：
   (数据实时同步facejoking)',"url"=>'',"picurl"=>'',"description"=>'');
-  for($id=0;$id<8;$id++){
-    $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
-    $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
-    $name = $xiaohua[data][$id][name];
-    $rank = $xiaohua[data][$id][rank];
-    $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
-  }
-  $b[]=array("title"=>'点击此处查看更多
+    for($id=0;$id<8;$id++){
+        $pic = "http://www.facejoking.com/pic/{$xiaohua[data][$id][pid]}.jpg";
+        $url = "http://www.facejoking.com/people/{$xiaohua[data][$id][pid]}";
+        $name = $xiaohua[data][$id][name];
+        $rank = $xiaohua[data][$id][rank];
+        $b[]=array("title"=>'TOP'.$rank.' '.$name,"url"=>$url,"picurl"=>$pic,"description"=>'');
+    }
+    $b[]=array("title"=>'点击此处查看更多
 你还可以回复【校花】查看校花排名
 ---------------
 [输入0返回主页]',"url"=>'http://www.facejoking.com/top/2016/1/',"picurl"=>'',"description"=>'');
-    return $this->respNews($b); 
+    return $this->respNews($b);
 
 }
 else return $this->respText('error');
